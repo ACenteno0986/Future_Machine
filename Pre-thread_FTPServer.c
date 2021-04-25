a4f55049b6ebabefa74161df3e8d714c14e89f85
Rye0567 <alecenteno72@gmail.com>
2021-04-24 21:27:31 -0600

    Conexión con el sevidor para un solo hilo.

--- Pre-thread_FTPServer.c
+++ Pre-thread_FTPServer.c
@@ -1,54 +1,125 @@
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include<sys/stat.h>
-#include<sys/sendfile.h>
-#include<fcntl.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <limits.h>
 #include <unistd.h>
-
+#include <pthread.h>
 
 int conexiones, puerto;
 char ruta[1024];
+struct sockaddr_in server, client;
+struct stat obj;
+int sock1, sock2;
+char buf[100], command[5], filename[20];
+int k, i, size, len, c;
+int filehandle;
+pthread_t *hilos;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 
+struct nodo{
+    struct nodo* sig;
+    int *cliente_socket;
+};
+typedef struct nodo nodo_t;
+
+nodo_t* cabeza = NULL;
+nodo_t* cola = NULL;
+
+void encolar(int* cliente_socket){
+    nodo_t *nuevonodo = malloc(sizeof(nodo_t));
+    nuevonodo->cliente_socket = cliente_socket;
+    nuevonodo->sig = NULL;
+    if(cola == NULL){
+        cabeza = nuevonodo;
+    }
+    else{
+        cola->sig = nuevonodo;
+    }
+    cola = nuevonodo;
+
+}
+
+int* desencolar(){
+    if(cabeza == NULL){
+        return NULL;
+    }
+    else{
+        int* resultado = cabeza->cliente_socket;
+        nodo_t* temporal = cabeza;
+        cabeza = cabeza->sig;
+        if(cabeza == NULL){
+            cola = NULL;
+        }
+        free(temporal);
+        return resultado;
+    }
+}
+
+
+void crear_hilos(){
+	
+}
+
+void* funcion_hilo(void*arg){
+    
+    while (true){
+        
+
+        int* pcliente ;
+
+        pthread_mutex_lock(&mutex);
+        pcliente = desencolar();
+        pthread_mutex_unlock(&mutex);
+
+        if(pcliente != NULL){
+            ManejarConexion(pcliente);
+        }
+    }
+    pthread_exit(NULL);
+    
+}
 
 int main(int argc,char *argv[])
 {
 
-    int opcion;
+  int opcion ;
 
-    while((opcion= getopt(argc,argv,"n:w:p:")) != -1){
+	while((opcion = getopt(argc,argv,"n:w:p:")) != -1)
+	{
+	
+		switch (opcion)
+		{
+			case 'n'://numero de conexiones maximas
+				conexiones = atoi(optarg);
 
-        switch(opcion){
+			case 'w':// direccion root del servidor
+				strcpy(ruta, optarg); 
+				
+			case 'p':// puerto del servidor
+				puerto = atoi(optarg);
+            
+			}
+			printf("opcion: %d", opcion);
+	}
 
-          case('n'):
-            conexiones = atoi(optarg);
-            printf("%d Conexiones: ", conexiones);
+printf("Conexiones: %d", conexiones);
 
-          case('w'):
-            strcpy(ruta, optarg);
-
-          case('p'):
-            puerto = atoi(optarg);
-          
-        }
-    }
-
-  struct sockaddr_in server, client;
-  struct stat obj;
-  int sock1, sock2;
-  char buf[100], command[5], filename[20];
-  int k, i, size, len, c;
-  int filehandle;
   sock1 = socket(AF_INET, SOCK_STREAM, 0);
   if(sock1 == -1)
     {
       printf("Socket creation failed");
       exit(1);
     }
+  
   server.sin_port = puerto;
   server.sin_addr.s_addr = 0;
   k = bind(sock1,(struct sockaddr*)&server,sizeof(server));
@@ -57,6 +128,7 @@ int main(int argc,char *argv[])
       printf("Binding error");
       exit(1);
     }
+   
   k = listen(sock1,1);
   if(k == -1)
     {
@@ -65,7 +137,7 @@ int main(int argc,char *argv[])
     }
   len = sizeof(client);
   sock2 = accept(sock1,(struct sockaddr*)&client, &len);
-  i = 1;
+  i = 1; 
   while(1)
     {
       recv(sock2, buf, 100, 0);
